cfb8942e1669e309ac818785e7dc147356443f38
enue <nnp@car.ocn.ne.jp>
2019-03-01 15:25:36 +0900

    fix complie error

--- Assets/Test.cs
+++ Assets/Test.cs
@@ -33,7 +33,7 @@ public class Test : MonoBehaviour {
 
 		if(GUILayout.Button("ChangeText")){
 			string sampleText = "Unityマニュアルガイドは特定のプラットフォームにのみ適用されるセクションを含みます。\n自ら参照したいセクションを選択して下さい。プロットフォーム特有の情報は各ページの三角形の記号で示されるボタンにより展開して参照することが出来ます。";
-			hypJpn.SetText(sampleText);
+			hypJpn.GetText(sampleText);
 		}
 
 		GUILayout.EndArea();
